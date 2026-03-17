c531914fe1705f9abb2624960b05bb9746021aa1
Milo <102558488+DevMiloo@users.noreply.github.com>
2026-03-17 23:16:05 +0100

    Create VanityData Cache (#52)
    * Create VanityData Cache
    
    Creates a cache for Vanity data so it does not fetch vanity data on every join, causing a rate limit. If cache is within 5 minutes it will use the cache instead of refetching, thus preventing rate limits.
    
    * Removes data from dinivte.js
    
    Fixes lint issue with unused data variable in dinvite.js

--- js/dinvite.js
+++ js/dinvite.js
@@ -12,6 +12,9 @@ module.exports = class InviteManager {
 
     client.invites = new Collection();
 
+    const vanityCache = {};
+    const cache_TTL = 5 * 60 * 1000;
+
     function hasInvitePermission(guild) {
       const hasPermission = guild.members.me?.permissions.has("ManageGuild");
       if (!hasPermission) {
@@ -129,7 +132,7 @@ module.exports = class InviteManager {
 
       if (hasVanityFeature && member.guild.vanityURLCode) {
         try {
-          vanityURL = await member.guild.fetchVanityData();
+          vanityURL = await getVanityURL(member.guild);
         } catch {
           vanityURL = null;
         }
@@ -208,5 +211,28 @@ module.exports = class InviteManager {
         );
       }
     });
+    
+    async function getVanityURL(guild) {
+      const now = Date.now();
+      const cache = vanityCache[guild.id];
+      
+      if (cache && now - cache.timestamp < cache_TTL) {
+        return cache.data;
+      }
+
+      let vanityURL = null;
+      try {
+        vanityURL = await guild.fetchVanityData();
+      } catch {
+        vanityURL = null;
+      }
+
+      vanityCache[guild.id] = {
+        data: vanityURL,
+        timestamp: now,
+      }
+
+      return vanityURL;
+    }
   }
 };
