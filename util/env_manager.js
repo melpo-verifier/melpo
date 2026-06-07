18b2979be4202c75a3cc9977f33608fa9efa6d5c
mathew653 <mathew653@users.noreply.github.com>
2026-06-07 21:59:56 +0100

    Commited console_hooks and env_manager module in prep.

---
+++ util/env_manager.js
@@ -0,0 +1,79 @@
+//============================
+//Name        : env_manager.js
+//Usage       : A container class for wrapping the loading of .env
+//============================
+
+//Data reference
+//----------------------------
+//Var   : #cfg.flags - Flags var.
+//0x01    Set upon successfully loading the .env file.
+//0x02    Cleared upon an error occuring.
+//0x04    Enable environment var caching(Usful for ensuring this class can recall its state across process boundaries).
+//----------------------------
+//Var   : #cfg.env_name - Environment varrible used to store state, can be changed to prevent namespace conflicts if any.
+//----------------------------
+
+/**
+ * env_manager
+ * - Handles reading of .env file
+ * - Can be set output its state to the environment to persist over boundaries.
+ * ***
+ * [See file for options](env_manager.js)
+ */
+class env_manager {
+    /**
+     * Internal configuration vars:  
+     * `flags`    - Configuration flags.  
+     * `env_name` - Environment varible name to use if environment caching is enabled.  
+     */
+    static #cfg = {
+        //flags   : (0x00 | 0x02),
+        flags   : (0x00 | 0x02 | 0x04),
+        env_name: "EVM_LAST_STATE"
+    };
+
+    /**
+     * Attempt to read .env and report error as needed.
+     * @returns {void}
+     */
+    static config() {
+        //Env cache mode : Run parity check against environment var before running.
+        if (env_manager.#cfg.flags & 0x04) {
+            if (
+                (typeof(process.env[env_manager.#cfg.env_name]) !== "undefined") &&
+                (env_manager.#cfg.flags != parseInt(process.env[env_manager.#cfg.env_name]))
+            )                                                                                   // Environment var existing and different from our status?
+            { env_manager.#cfg.flags = parseInt(process.env[env_manager.#cfg.env_name]); }      // Adopt it as our new status and continue.
+        }
+
+        if (!(env_manager.#cfg.flags & 0x01))          //If our first load has been done, we do not need to reparse.
+        {
+            //require("dotenv").config();
+            try { 
+                process.loadEnvFile();                 //Try loading the .env file using node.js's native callback.
+                console.log("Loaded .env file.");      //Notify on success.
+                env_manager.#cfg.flags |= 0x01;        //Set a flag that we loaded properly, as we do not need to repeat read.
+            } 
+            catch (error) 
+            { 
+                if (env_manager.#cfg.flags & 0x02)
+                {
+                    console.error("Failed to load .env file.\n", error); 
+                    env_manager.#cfg.flags ^= 0x02;     //xor off the bit as we have already errored.
+                }
+            }
+        }
+
+        //Env cache mode : Sync flags to a local environment var so we can track our state inside a child process/inhirreted environment block.
+        if (env_manager.#cfg.flags & 0x04) { process.env[env_manager.#cfg.env_name]=String(env_manager.#cfg.flags) }
+    }
+
+    /**
+     * Utility function for checking if a environment var exists.
+     * @param {String} elem Environment var to look for as a string.
+     * @returns {Boolean} True if exists, false otherwise.
+     */
+    static has(elem) { return (typeof(process.env[elem]) !== "undefined"); }
+}
+
+module.exports=env_manager;
