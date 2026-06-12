f74ac7fb287d5078ef1d40cd401ac56f6d7353a3
mathew653 <mathew653@users.noreply.github.com>
2026-06-12 20:15:57 +0100

    Fixed indentation in new console_hooks and env_manager class. Split application default logic from migration/migrateToApplications.js and models/Application.js Implemented Shared template for application default logic at templates/application_defaults.js

--- models/Application.js
+++ models/Application.js
@@ -1,36 +1,5 @@
 //--Default embeds--
-const def_embeds = {
-  welcome:{ 
-    title: "Welcome {username}!", 
-    description: "Hello {usermention}, welcome to **${interaction.guild.name}**!", 
-    color: "#3f7ff1" 
-  },
-  verify_sv_channel:{ 
-    title: "How to verify", 
-    description: `After clicking the "Apply" button below the bot will DM you some questions in order for you to access the server. You'll have to fill out the complete form in order for the moderators to see your application. \n\nClick the "Apply" button below to start the application`, 
-    color: "#3f7ff1" 
-  },
-  verify_dm_accepted:{ 
-    title: `Application accepted`, 
-    description: "Your application for **{appName}** in **${interaction.guild.name}** has been accepted by {modname}!", 
-    color: "#008000" 
-  },
-  verify_dm_deny:{ 
-    title: `Application Denied`, 
-    description: "Your application for **{appName}** in **${interaction.guild.name}** has been denied by {modname}!", 
-    color: "#EB2121" 
-  },
-  verify_dm_start:{ 
-    title: "${interaction.guild.name}'s Verification", 
-    description: '**Welcome to Melpo\'s verification!**\nWelcome {username} to the verification process of ${interaction.guild.name}! Please answer the following questions within 60 minutes. You can cancel the verification any time by clicking "cancel".', 
-    color: "#3f7ff1" 
-  },
-  verify_dm_finish:{
-    title: `Application Completed`, 
-    description: "Your application has been completed successfully and has been sent to review to ${interaction.guild.name}!", 
-    color: "#008000" 
-  }
-};
+var def_embeds=require("../templates/application_defaults.js");
 
 //--Application DB export--
 module.exports = (sequelize, DataTypes) => {
--- util/console_hooks.js
+++ util/console_hooks.js
@@ -33,130 +33,130 @@
  * [See file for options](console_hooks.js)
  */
 class console_hooks {
-    /**
-     * Internal configuration vars.  
-     * `flags`  - Configuration flags.  
-     * `prefix` - String to contain prefix when enabled.  
-     */
-    static #cfg = {
-        //flags: (0x01 | 0x02 | 0x04)   //Stack trace for function names(error and log).
-        flags: (0x00 | 0x08),           //Disable stack tracing for function names, use console prefix.
-        prefix: ""                      //Console prefix if applicable.
-    };
+  /**
+   * Internal configuration vars.  
+   * `flags`  - Configuration flags.  
+   * `prefix` - String to contain prefix when enabled.  
+   */
+  static #cfg = {
+    //flags: (0x01 | 0x02 | 0x04)   //Stack trace for function names(error and log).
+    flags: (0x00 | 0x08),           //Disable stack tracing for function names, use console prefix.
+    prefix: ""                      //Console prefix if applicable.
+  };
 
-    /**
-     * Place a copy to the handle of the original call backs into a list.  
-     * ***
-     * Note : [Console reference](https://console.spec.whatwg.org/#console-namespace) for standards(log,error and warn).  
-     */
-    static #def_funcs = {
-        con_log: console.log,
-        con_err: console.error,
-        con_wrn: console.warn
-    };
+  /**
+   * Place a copy to the handle of the original call backs into a list.  
+   * ***
+   * Note : [Console reference](https://console.spec.whatwg.org/#console-namespace) for standards(log,error and warn).  
+   */
+  static #def_funcs = {
+    con_log: console.log,
+    con_err: console.error,
+    con_wrn: console.warn
+  };
 
-    /**
-     * Parameters for ansi spec color selection.  
-     * `col_info`  - Info Tag Color.  
-     * `col_error` - Error Tag Color.  
-     * `col_warn`  - Warning Tag Color.  
-     * ***
-     * Cite : [Ansi specs](https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit) for standards  
-     * Note : Discord's ansi code block also supports 30-37 [see here](https://gist.github.com/kkrypt0nn/a02506f3712ff2d1c8ca7c9e0aed7c06)  
-     */
-    static #tag_colors = {
-        col_info: "34",
-        col_error: "31",
-        col_warn: "33"
-    };
+  /**
+   * Parameters for ansi spec color selection.  
+   * `col_info`  - Info Tag Color.  
+   * `col_error` - Error Tag Color.  
+   * `col_warn`  - Warning Tag Color.  
+   * ***
+   * Cite : [Ansi specs](https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit) for standards  
+   * Note : Discord's ansi code block also supports 30-37 [see here](https://gist.github.com/kkrypt0nn/a02506f3712ff2d1c8ca7c9e0aed7c06)  
+   */
+  static #tag_colors = {
+    col_info: "34",
+    col_error: "31",
+    col_warn: "33"
+  };
 
-    //--Interface--
+  //--Interface--
 
-    /**
-     * Adds a prefix tag to console events
-     * @param {string|optional} prefix tag to assign or clear if empty/not specified
-     */
-    static SetPrefix(prefix = "") { console_hooks.#cfg.prefix = prefix; }
+  /**
+   * Adds a prefix tag to console events
+   * @param {string|optional} prefix tag to assign or clear if empty/not specified
+   */
+  static SetPrefix(prefix = "") { console_hooks.#cfg.prefix = prefix; }
 
-    //--hooks--
-    //Note : Directly call reference to reference functions inside hooks to prevent infinate recursion.
-    //Note : Trace logic is repeated inside functions as not to have it effect stack.
+  //--hooks--
+  //Note : Directly call reference to reference functions inside hooks to prevent infinate recursion.
+  //Note : Trace logic is repeated inside functions as not to have it effect stack.
 
-    /**
-     * Hook : Console.log interception.
-     */
-    static hook_log() {
-        let _args = Array.prototype.slice.call(arguments);                                                   //Convert provided arguements to array.
-        //--Start tag prefixing logic--
-        {
-            let _tag = "[\x1b[" + console_hooks.#tag_colors.col_info + "m";                                  //Write start of tag(set color attribute)
-            if (console_hooks.#cfg.flags & 0x01) {
-                let _err = new Error(); //Note : Expensive but only way to walk the stack in strict mode.
-                console_hooks.#def_funcs.con_log.apply(console, ["[Trace]", _err.stack?.split("\n")[2]]);    //If in trace mode output where this function is called.
-                _tag += _err.stack?.split("\n")[2]?.trim().split(" ")[1] + " | ";                            //If in trace mode prefix function name.
-            }
-            _tag += "info\x1b[0m]";                                                                          //Write end of tag(reset color attribute)
-            if ((console_hooks.#cfg.prefix.length > 0) && (console_hooks.#cfg.flags & 0x08))
-            { _tag+="["+console_hooks.#cfg.prefix+"]"; }                                                     //Append prefix if enabled and present.
-            _args[0] = _tag + _args[0];                                                                      //Append tag to first arguement in array(this keeps log formatting working.)
-        }
+  /**
+   * Hook : Console.log interception.
+   */
+  static hook_log() {
+    let _args = Array.prototype.slice.call(arguments);                                                   //Convert provided arguements to array.
+    //--Start tag prefixing logic--
+    {
+      let _tag = "[\x1b[" + console_hooks.#tag_colors.col_info + "m";                                  //Write start of tag(set color attribute)
+      if (console_hooks.#cfg.flags & 0x01) {
+        let _err = new Error(); //Note : Expensive but only way to walk the stack in strict mode.
+        console_hooks.#def_funcs.con_log.apply(console, ["[Trace]", _err.stack?.split("\n")[2]]);    //If in trace mode output where this function is called.
+        _tag += _err.stack?.split("\n")[2]?.trim().split(" ")[1] + " | ";                            //If in trace mode prefix function name.
+      }
+      _tag += "info\x1b[0m]";                                                                          //Write end of tag(reset color attribute)
+      if ((console_hooks.#cfg.prefix.length > 0) && (console_hooks.#cfg.flags & 0x08))
+      { _tag+="["+console_hooks.#cfg.prefix+"]"; }                                                     //Append prefix if enabled and present.
+      _args[0] = _tag + _args[0];                                                                      //Append tag to first arguement in array(this keeps log formatting working.)
+    }
         //--End tag prefixing logic--
 
-        console_hooks.#def_funcs.con_log.apply(console, _args);
+    console_hooks.#def_funcs.con_log.apply(console, _args);
+  }
+
+  /**
+   * Hook : Console.error interception.
+   */
+  static hook_err() {
+    let _args = Array.prototype.slice.call(arguments);                                                  //Convert provided arguements to array.
+    //--Start tag prefixing logic--
+    {
+      let _tag = "[\x1b[" + console_hooks.#tag_colors.col_error + "m";                                //Write start of tag(set color attribute)
+      if (console_hooks.#cfg.flags & 0x02) {
+        let _err = new Error(); //Note : Expensive but only way to walk the stack in strict mode.
+        console_hooks.#def_funcs.con_err.apply(console, ["[Trace]", _err.stack?.split("\n")[2]]);   //If in trace mode output where this function is called.
+        _tag += _err.stack?.split("\n")[2]?.trim().split(" ")[1] + " | ";                           //If in trace mode prefix function name.
+      }
+      _tag += "error\x1b[0m]";                                                                        //Write end of tag(reset color attribute)
+      if ((console_hooks.#cfg.prefix.length > 0) && (console_hooks.#cfg.flags & 0x08))
+      { _tag+="["+console_hooks.#cfg.prefix+"]"; }                                                    //Append prefix if enabled and present.
+      _args[0] = _tag + _args[0];                                                                     //Append tag to first arguement in array(this keeps log formatting working.)
     }
+    //--End tag prefixing logic--
 
-    /**
-     * Hook : Console.error interception.
-     */
-    static hook_err() {
-        let _args = Array.prototype.slice.call(arguments);                                                  //Convert provided arguements to array.
-        //--Start tag prefixing logic--
-        {
-            let _tag = "[\x1b[" + console_hooks.#tag_colors.col_error + "m";                                //Write start of tag(set color attribute)
-            if (console_hooks.#cfg.flags & 0x02) {
-                let _err = new Error(); //Note : Expensive but only way to walk the stack in strict mode.
-                console_hooks.#def_funcs.con_err.apply(console, ["[Trace]", _err.stack?.split("\n")[2]]);   //If in trace mode output where this function is called.
-                _tag += _err.stack?.split("\n")[2]?.trim().split(" ")[1] + " | ";                           //If in trace mode prefix function name.
-            }
-            _tag += "error\x1b[0m]";                                                                        //Write end of tag(reset color attribute)
-            if ((console_hooks.#cfg.prefix.length > 0) && (console_hooks.#cfg.flags & 0x08))
-            { _tag+="["+console_hooks.#cfg.prefix+"]"; }                                                    //Append prefix if enabled and present.
-            _args[0] = _tag + _args[0];                                                                     //Append tag to first arguement in array(this keeps log formatting working.)
-        }
-        //--End tag prefixing logic--
+    console_hooks.#def_funcs.con_err.apply(console, _args);
+  }
 
-        console_hooks.#def_funcs.con_err.apply(console, _args);
+  /**
+   * Hook : Console.warn interception.
+   */
+  static hook_wrn() {
+    let _args = Array.prototype.slice.call(arguments);                                                 //Convert provided arguements to array.
+    //--Start tag prefixing logic--
+    {
+      let _tag = "[\x1b[" + console_hooks.#tag_colors.col_warn + "m";                                //Write start of tag(set color attribute)
+      if (console_hooks.#cfg.flags & 0x04) {
+        let _err = new Error(); //Note : Expensive but only way to walk the stack in strict mode.
+        console_hooks.#def_funcs.con_wrn.apply(console, ["[Trace]", _err.stack?.split("\n")[2]]);  //If in trace mode output where this function is called.
+        _tag += _err.stack?.split("\n")[2]?.trim().split(" ")[1] + " | ";                          //If in trace mode prefix function name.
+      }
+      _tag += "warn\x1b[0m]";                                                                        //Write end of tag(reset color attribute)
+      if ((console_hooks.#cfg.prefix.length > 0) && (console_hooks.#cfg.flags & 0x08))
+      { _tag+="["+console_hooks.#cfg.prefix+"]"; }                                                   //Append prefix if enabled and present.
+      _args[0] = _tag + _args[0];                                                                    //Append tag to first arguement in array(this keeps log formatting working.)
     }
+    //--End tag prefixing logic--
 
-    /**
-     * Hook : Console.warn interception.
-     */
-    static hook_wrn() {
-        let _args = Array.prototype.slice.call(arguments);                                                 //Convert provided arguements to array.
-        //--Start tag prefixing logic--
-        {
-            let _tag = "[\x1b[" + console_hooks.#tag_colors.col_warn + "m";                                //Write start of tag(set color attribute)
-            if (console_hooks.#cfg.flags & 0x04) {
-                let _err = new Error(); //Note : Expensive but only way to walk the stack in strict mode.
-                console_hooks.#def_funcs.con_wrn.apply(console, ["[Trace]", _err.stack?.split("\n")[2]]);  //If in trace mode output where this function is called.
-                _tag += _err.stack?.split("\n")[2]?.trim().split(" ")[1] + " | ";                          //If in trace mode prefix function name.
-            }
-            _tag += "warn\x1b[0m]";                                                                        //Write end of tag(reset color attribute)
-            if ((console_hooks.#cfg.prefix.length > 0) && (console_hooks.#cfg.flags & 0x08))
-            { _tag+="["+console_hooks.#cfg.prefix+"]"; }                                                   //Append prefix if enabled and present.
-            _args[0] = _tag + _args[0];                                                                    //Append tag to first arguement in array(this keeps log formatting working.)
-        }
-        //--End tag prefixing logic--
+    console_hooks.#def_funcs.con_wrn.apply(console, _args);
+  }
 
-        console_hooks.#def_funcs.con_wrn.apply(console, _args);
-    }
-
-    //Using a static initialisation block here, so when the class is defined it will automatically hook the console object.
-    static {
-        console.log   = console_hooks.hook_log;
-        console.error = console_hooks.hook_err;
-        console.warn  = console_hooks.hook_wrn;
-    }
+  //Using a static initialisation block here, so when the class is defined it will automatically hook the console object.
+  static {
+    console.log   = console_hooks.hook_log;
+    console.error = console_hooks.hook_err;
+    console.warn  = console_hooks.hook_wrn;
+  }
 }
 
 module.exports=console_hooks;
