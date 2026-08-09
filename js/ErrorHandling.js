6028d1f4f251ad1956e2a6705acd0ef1ef4a4a83
Mathew! <mathew_653@nomail.com>
2026-08-09 15:22:06 +0100

    Fixed condition where invalid channel/user handle could produce a error.

--- js/ErrorHandling.js
+++ js/ErrorHandling.js
@@ -217,18 +217,29 @@ class ErrorHandler {
 			//Diagnostic handler block.
 			const debug_actions = {
 				act_DM: async (_out) => {
-					const devUser = await client.users.fetch(process.env.BUG_REPORT_USER);
-					await devUser.send(_out);
+					const Fail_function = function (error) {
+						console.error("Failed to fetch DM for direct message report(Tried %s).", process.env.BUG_REPORT_USER);
+					};
+
+					const devUser = await client.users.fetch(process.env.BUG_REPORT_USER).catch(Fail_function);
+					if (devUser !== null) await devUser?.send(_out);
 				},
 				act_CH: async (_out) => {
-					const devChan = await client.channels.fetch(process.env.BUG_REPORT_CHAN);
-					//Forum/Thread unarchive if needs be(Only needed for non webhooks, as archived threads complain if a post is attempted).
-					if (devChan.isThread()) {
-						if (devChan.archived) await devChan.setArchived(false);
-					}
+					const Fail_function = function (error) {
+						console.error("Failed to fetch debug channel for report(Tried %s).", process.env.BUG_REPORT_CHAN);
+					};
 
-					//Everything checked, send as normal.
-					await devChan.send(_out);
+					const devChan = await client.channels.fetch(process.env.BUG_REPORT_CHAN).catch(Fail_function);
+					if (devChan !== null) {
+						//Forum/Thread unarchive if needs be(Only needed for non webhooks, as archived threads complain if a post is attempted).
+						//Note : Using optional chain here to ensure if channel is null this will properly eval to false.
+						if (devChan?.isThread()) {
+							if (devChan.archived) await devChan.setArchived(false);
+						}
+
+						//Everything checked, send as normal.
+						await devChan?.send(_out);
+					}
 				},
 			};
 
